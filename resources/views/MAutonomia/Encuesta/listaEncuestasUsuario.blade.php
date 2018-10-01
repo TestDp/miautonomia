50e58b77304f9a5018061ffd409ef342d01e9371
diegodpsoluciones <33739109+diegodpsoluciones@users.noreply.github.com>
2018-10-01 17:51:04 -0500

    Actualizaciones de producción

--- resources/views/MAutonomia/Encuesta/listaEncuestasUsuario.blade.php
+++ resources/views/MAutonomia/Encuesta/listaEncuestasUsuario.blade.php
@@ -11,17 +11,19 @@
 									<span class="image"><img style="width: 100%;" src="images/arbol-left.png" alt="" /></span>
 								</header>	
 								</div>
-								<div class="col-4 col-12-medium">
-									<div style="text-align: center; color: #8d562f; font-weight: 700;" class="widget-shadow">
+									<div class="col-4 col-12-medium">
+								@foreach($encuestas as $encuesta)
+									<div style="margin-top:2%; margin-bottom:2%; text-align: center; color: #8d562f; font-weight: 700;" class="widget-shadow">
 										<div style="background-size: cover; background-image: url('{{ asset('images/pic02.jpg') }}');" class="login-body">
 										<img class="media-object" style="width:100%;" src="{{ asset('images/hojas-amarillas.png') }}"></img>
-											@foreach($encuestas as $encuesta)
+											
 												<fieldset>
 													<a href="{{url('FormularioEncuesta', ['idEncuesta' => $encuesta->id ])}}">{{ $encuesta->NombreEncuesta }}</a>
 												</fieldset>
-											@endforeach
+											
 										</div>
 									</div>	
+									@endforeach
 								</div>
 								<div class="col-4 col-12-medium">
 										<span class="image"><img style="width: 100%;" src="images/arbol-right.png" alt="" /></span>
