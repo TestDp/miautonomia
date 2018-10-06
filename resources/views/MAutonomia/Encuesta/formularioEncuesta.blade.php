5d2a37cf2636d068427f9344391c9786caf266c3
diegodpsoluciones <33739109+diegodpsoluciones@users.noreply.github.com>
2018-10-06 13:51:15 -0500

    gatico seleccionador

--- resources/views/MAutonomia/Encuesta/formularioEncuesta.blade.php
+++ resources/views/MAutonomia/Encuesta/formularioEncuesta.blade.php
@@ -12,34 +12,53 @@
 							<div class="row">
 								<div class="col-12-medium">
 									<fieldset name="RespuestasUsuario">
-										<div style="font-weight: 700; color: #ffffff; background: #e86e48; padding: 5%; border-radius: 50px;" name ="id_pregunta" value = "{{ $PreguntasFormulario->id }}"> ¿ {{ $PreguntasFormulario->Enunciado }} ?
+										<div style="font-weight: 700; color: #ffffff; background: #e86e48; padding: 1%; border-radius: 50px;" name ="id_pregunta" value = "{{ $PreguntasFormulario->id }}"> ¿ {{ $PreguntasFormulario->Enunciado }} ?
 										</div>
 										@foreach($PreguntasFormulario->Respuestas as $respuestas)
 											<div class="col-sm-2 col-sm-offset-1" >
 
-												<label><input type="radio" onclick="guardarRespuestaUsuario(this,{{$respuestas->id}})" id="Respuesta_id" name="Respuesta_id[{{$loop->parent->index}}]" data-toggle="modal" data-target="#modalExplicacionRespuesta{{$respuestas->id}}">
-													<b>{{$respuestas->Descripcion}}</b>
+												<label><input data-num="{{$respuestas->Puntaje}}" type="radio" onclick="guardarRespuestaUsuario(this,{{$respuestas->id}})" id="Respuesta_id" name="Respuesta_id[{{$loop->parent->index}}]" data-toggle="modal" data-target="#modalExplicacionRespuesta{{$respuestas->id}}">
+													<b>{{$respuestas->Descripcion}}
 													@if($loop->index == 0)
-														<img class="plumas" style="width: 40%; display:block; margin:auto;" src="{{ asset('images/verde.png') }}"></img>
+														<div class="image-container">
+															<img class="plumas" style="width: 30%; display:block; margin:auto;" src="{{ asset('images/verde.png') }}"></img>
+															<div class="after"><img class="media-object" style="width:100%; display:block; margin:auto;" src="{{ asset('images/gato-feliz.gif') }}"></img></div>
+														</div>
 													@else
 														@if($loop->index == 1)
-															<img class="plumas" style="width: 40%; display:block; margin:auto;" src="{{ asset('images/naranja.png') }}"></img>
+														<div class="image-container">
+															<img class="plumas" style="width: 30%; display:block; margin:auto;" src="{{ asset('images/naranja.png') }}"></img>
+															<div class="after"><img class="media-object" style="width:100%; display:block; margin:auto;" src="{{ asset('images/gato-feliz.gif') }}"></img></div>
+														</div>
 														@else
 															@if($loop->index == 2)
-																<img class="plumas" style="width: 40%; display:block; margin:auto;" src="{{ asset('images/amarilla.png') }}"></img>
+															<div class="image-container">
+																<img class="plumas" style="width: 30%; display:block; margin:auto;" src="{{ asset('images/amarilla.png') }}"></img>
+																<div class="after"><img class="media-object" style="width:100%; display:block; margin:auto;" src="{{ asset('images/gato-feliz.gif') }}"></img></div>
+															</div>
 															@else
 																@if($loop->index == 3)
-																	<img class="plumas" style="width: 40%; display:block; margin:auto;" src="{{ asset('images/morada.png') }}"></img>
+																<div class="image-container">
+																		<img class="plumas" style="width: 30%; display:block; margin:auto;" src="{{ asset('images/morada.png') }}"></img>
+																		<div class="after"><img class="media-object" style="width:100%; display:block; margin:auto;" src="{{ asset('images/gato-feliz.gif') }}"></img></div>
+																</div>
 																@else
 																	@if($loop->index == 4)
-																		<img class="plumas" style="width: 40%; display:block; margin:auto;" src="{{ asset('images/azul.png') }}"></img>
+																	<div class="image-container">
+																		<img class="plumas" style="width: 30%; display:block; margin:auto;" src="{{ asset('images/azul.png') }}"></img>
+																		<div class="after"><img class="media-object" style="width:100%; display:block; margin:auto;" src="{{ asset('images/gato-feliz.gif') }}"></img></div>
+																	</div>
 																	@else
-																		<img class="plumas" style="width: 40%; display:block; margin:auto;" src="{{ asset('images/verde.png') }}"></img>
+																	<div class="image-container">
+																		<img class="plumas" style="width: 30%; display:block; margin:auto;" src="{{ asset('images/verde.png') }}"></img>
+																		<div class="after"><img class="media-object" style="width:100%; display:block; margin:auto;" src="{{ asset('images/gato-feliz.gif') }}"></img></div>
+																	</div>
 																	@endif
 																@endif
 															@endif
 														@endif
 													@endif
+													</b>
 												</label>
 												<!-- Modal confirmación elimminar respuesta-->
 												@if($respuestas->Puntaje >= 0)
@@ -51,6 +70,7 @@
 																	<div class="row">
 																		<div class="col-md-6">
 																			<div class="modal-header">
+																				<img class="swing" style="display:block; margin:auto; width: 40%; left: 25%; top: -20px; position: absolute;" src="{{ asset('images/sombrero.png') }}"></img>
 																				<img class="media-object" style="width:100%; display:block; margin:auto;" src="{{ asset('images/gato-feliz.gif') }}"></img>
 																				<img class="ball" style="display:block; margin:auto;" src="{{ asset('images/pelota.png') }}"></img>
 																			</div>
@@ -109,8 +129,8 @@
 											</div>
 										@endforeach
 										<label id="respuestaSinResponder" name="respuestaSinResponder"></label>
-										<img class="media-object" style="width:20%; display:block; margin:auto;" src="{{ asset('images/ojos.gif') }}"></img>
-										<div style="border-bottom: 5px solid #e86e48; width: 40%; display: block; margin: auto;"></div>
+										<!--<img class="media-object" style="position: relative; padding-top: 15%; width:20%; display:block; margin:auto;" src="{{ asset('images/ojos.gif') }}"></img>-->
+										<!--<div style="border-bottom: 5px solid #e86e48; width: 40%; display: block; margin: auto;"></div>-->
 									</fieldset>
 								</div>
 							</div>
